c9550e694cf870413af4449a08f8f89daefa84f1
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-11-29 22:05:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from flask import Flask, request, render_template
+
+chrome_options = Options()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/<url>')
+def ss(url):
+    try:
+        browser.get(url)
+        sleep(2)
+        browser.get_screenshot_as_file("static/screenshot.png")
+        browser.quit()
+        
+        return render_template("ss.html")
+    except:
+        return "Website does not exist!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+        
+    
+
