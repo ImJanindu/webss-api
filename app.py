43ba9af9d1c7511d57eb39138482b78dedd52655
Janindu Malshan <janindumalshan50@gmail.com>
2026-09-18 23:00:39 +0530

    refactor: setup fresh browser sessions for each requests

--- app.py
+++ app.py
@@ -8,13 +8,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-chrome_options = Options()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
 @app.route('/')
 def index():
     return "Hemlo!"
@@ -22,14 +15,24 @@ def index():
 @app.route('/<url>')
 def ss(url):
     try:
+        chrome_options = Options()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
         abc = "https://" + url
         browser.set_window_size(1920, 1080)
         browser.get(abc)
         sleep(2)
         browser.get_screenshot_as_file("static/screenshot.png")
         response = upload_file("static/screenshot.png")
+        browser.quit()
         return f"https://telegra.ph{response[0]}"
-    except:
+    except Exception as e:
+        if 'browser' in locals():
+            browser.quit()
         return "None"
 
 
